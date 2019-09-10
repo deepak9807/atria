01f1b8502e8111e21133251f71158795d84435fc
Deepak Chaube <deepakchaube01@gmail.com>
2019-09-10 17:19:21 +0530

    initial commit

---
+++ atria_backend/sensor/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class SensorData(models.Model):
+    sensor_type = models.CharField(max_length=255, null=True, blank=True)
+    reading = models.FloatField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.sensor_type}::{self.reading}'
+
+    def __repr__(self):
+        return f'SensorData({self.sensor_type}:{self.reading})'
+
+    class Meta:
+        ordering = ['-updated_at']
---
+++ atria_backend/sensor/views.py
@@ -0,0 +1,11 @@
+from django_rest_framwork import APIView
+
+class SensorDataView(APIView):
+
+    def post(self, request):
+        pass
+    
+    def get(self, request):
+        pass
+
+
